19a12db682ac182eb2b6c47cd9273f4804fc2514
deeksha01032023 <deekshapandey@cedcoss.com>
2023-06-23 17:11:51 +0530

    done

--- src/index.php
+++ src/index.php
@@ -1,127 +1,17 @@
-<?php
-$data = file_get_contents('array.json');
-$data = json_decode($data, true);
-?>
-<?php
-// function to generate Table
-function generateTable($data)
-{
-  echo "<table border='1'>";
-  echo "<thead>";
-  echo "<tr>";
-  echo "<th rowspan='3'>Time</th>";
-  foreach ($data as $quarter => $inquarter) {
-    foreach ($inquarter as $location => $inlocation) {
-      echo "<th colspan = '3'> Location : $location </th>";
-    }
-    echo "</tr>";
-    break;
-  }
-  echo "</tr> <tr></tr> <tr>";
-
-  foreach ($data as $quarter => $inquarter) {
-    foreach ($inquarter as $location => $inlocation) {
-      foreach ($inlocation as $item => $count) {
-        echo "<th> $item </th>";
-      }
-    }
-    echo "</tr>";
-    break;
-  }
-  echo "</tr> </thead>";
-
-  echo "<tbody>";
-  foreach ($data as $quarter => $inquarter) {
-    echo "<tr> <td> $quarter </td>";
-    foreach ($inquarter as $location => $inlocation) {
-      foreach ($inlocation as $item => $count) {
-        echo "<td> $count </td>";
-      }
-    }
-    echo "</tr>";
-  }
-  echo "</tbody></table>";
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script type="text/javascript" src="data.js"></script>
-  <link rel="stylesheet" href="./CSS/style.css">
-  <title>Multidimensional Array</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/style.css">
+    
+    <title>Number to word</title>
 </head>
-
 <body>
-  <!-- 1. Display the data in tabular form -->
-  <h2> Display the data in tabular form</h2>
-  <?php generateTable($data); ?>
-  <!-- 2. Identify the quarter with maximum sale of egg. -->
-  <hr>
-  <h2>Identify the quarter with maximum sale of egg.</h2>
-  <?php
-  $max_egg = 0;
-  $egg = "Q1";
-
-  foreach ($data as $quarter => $inQuarter) {
-    $temp_quarter = $quarter;
-    $temp_egg = 0;
-    foreach ($inQuarter as $location => $inLocation) {
-      $temp_egg += $inLocation['Egg'];
-    }
-    if ($temp_egg > $max_egg) {
-      $max_egg = $temp_egg;
-      $egg = $temp_quarter;
-    }
-  }
-  echo "Quarter with maximum sales of egg is : <b> $egg
-   </b> with total of <b> $max_egg </b> sales <br>";
-  ?>
-  <!-- 3.Identify the location with minimum consumption of milk. -->
-  <hr>
-  <h2>Identify the location with minimum consumption of milk.</h2>
-  <?php
-  $milk_sales = array(
-    "Kolkata" => 0,
-    "Delhi" => 0,
-    "Mumbai" => 0
-  );
-  foreach ($data as $quarter => $inQuarter) {
-    foreach ($inQuarter as $location => $inLocation) {
-      $milk_sales[$location] += $inLocation['Milk'];
-    }
-  }
-  $milk_loc = array_search(min($milk_sales), $milk_sales);
-  echo "Location with minimum consumptioin of milk is : <b>
-   $milk_loc </b> with total of <b> $milk_sales[$milk_loc] </b>l. <br>";
-  ?>
-
-  <!-- Delete location with minimum sale of bread. -->
-  <hr>
-  <h2>Delete location with minimum sale of bread.</h2>
-  <?php
-  $bread_sales = array(
-    "Kolkata" => 0,
-    "Delhi" => 0,
-    "Mumbai" => 0
-  );
-  foreach ($data as $quarter => $inQuarter) {
-    foreach ($inQuarter as $location => $inLocation) {
-      $bread_sales[$location] += $inLocation['Bread'];
-    }
-  }
-  $bread_loc = array_search(min($bread_sales), $bread_sales);
-  echo "Location with minimum sales of bread is : <b> $bread_loc
-   </b> with total of <b> $bread_sales[$bread_loc] </b>l. <br>";
-  foreach ($data as $quarter => $inQuarter) {
-    unset($data[$quarter][$bread_loc]);
-  }
-  generateTable($data);
-  ?>
+    <h4 class = "heading">Please Enter the number : </h4>
+    <label for="number"><input id = "number" type="number" onkeyup = "toWord()"/></label>
+    <h4 id = "output"></h4>
 </body>
-
+<script src="./js/main.js"></script>
 </html>
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,113 @@
-$("#switch").click(function () {
-    var mycheck = ($("#switch").prop("checked") == true ? '1' : '0');
-    if (mycheck == 0) {
-        document.body.style.backgroundColor = 'white';
+const till20 = {
+    0 : "zero",
+    1 : "one",
+    2 : "two",
+    3 : "three",
+    4 : "four",
+    5 : "five",
+    6 : "six",
+    7 : "seven",
+    8 : "eight",
+    9 : "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen"
+};
+const after20 = {
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety"
+};
+
+const power = {
+    0 : "",
+    1 : "",
+    2 : " hundred",
+    3 : " thousand",
+    4 : " thousand",
+    5 : " lakh",
+    6 : " lakh",
+    7 : " crore",
+    8 : " crore"
+};
+// function to club two digits to make a 2 digit number
+function twoDigits(x, y) {
+    return x + 10 * y;
+}
+// when the number is less than 20
+function lessthan20(num, i) {
+    let ret = " ";
+    ret += till20[num];
+    ret += " ";
+    ret += power[i];
+    return ret;
+}
+// when the number is more than 20
+function morethan20(num, i) {
+    let ret = " ";
+
+    ret += after20[Math.floor(num/10) * 10];
+    if(Math.floor(num % 10) > 0) {
+        ret += " ";
+        ret += till20[Math.floor(num % 10)];
     }
-    else {
-        document.body.style.backgroundColor = 'black';
+    ret += " ";
+    ret += power[i];
+
+    return ret;
+}
+// main function to convert given number in word
+function toWord() {
+    let num = document.getElementById("number").value;
+    let val = num;
+        const arr = [];
+    while(num >= 1) {
+        arr.push(num % 10);
+        num = Math.floor(num/10);
     }
-   
-});
+    let ans = " only";
+    const n = arr.length;
+    let idx = 0;
+    for(let i = 0; i < n; i++) {
+        let dig;
+        if(i == 2) {
+            dig = twoDigits(arr[i], 0);
+        } else{
+            if(i + 1 < n) {
+                dig = twoDigits(arr[i], arr[++i]);
+            } else {
+                dig = twoDigits(arr[i], 0);
+            }
+        } 
+        if(dig < 1) continue;
+        if(dig < 20) {
+            ans = lessthan20(dig, i) + ans;
+        } else {
+            ans = morethan20(dig, i) + ans;
+        }
+    }
+    if(val < 0) {
+        document.getElementById("output").innerHTML = "please enter positive value";
+    } else {
+        if(val < 1) {
+            ans = "zero";
+        } 
+        if(val.length < 1) {
+            ans = "";
+        }
+        console.log(val);
+        document.getElementById("output").innerHTML = ans;
+    }
+}
